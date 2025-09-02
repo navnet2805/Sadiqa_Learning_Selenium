c96e369d84ebb38a49bef44202a7fa79611a1180
Naveen <naveen.rp.2805@gmail.com>
2025-09-02 23:34:37 +0530

    Commit 01

--- src/main/java/org/example/P01.java
+++ src/main/java/org/example/P01.java
@@ -1,4 +1,20 @@
 package org.example;
 
+import org.openqa.selenium.SearchContext;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
 public class P01 {
+    //launch the browser
+
+    public static void main(String[] args) {
+        WebDriver obj = new ChromeDriver(); //creating object of chrome driver and assigning the address of obj to its parent(WebDirver) class
+        obj.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login"); //get() - it is 1 param method - return type is void.
+        // it will create a GET request to the API .
+
+        System.out.println("hello world");
+        //obj.close();
+        obj.quit();
+    }
 }
